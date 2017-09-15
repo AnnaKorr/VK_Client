c4b49a9d238f3453d6b5c8931fa31f8b4a54b9b3
AnnaKorr <7319162@gmail.com>
2017-09-15 15:59:15 +0300

    Only Main storyBoard

---
+++ Shatrova_VkApp/LoginScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginScreenViewController.swift
+//  Shatrova_VkApp
+//
+//  Created by apple on 14.09.17.
+//  Copyright © 2017 Korona. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
